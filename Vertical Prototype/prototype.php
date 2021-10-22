c45df4cbf7e1cb7830d42e59a97b4eaf095b6628
BryceKurek <89614933+BryceKurek@users.noreply.github.com>
2021-10-22 18:33:53 -0400

    Update prototype.php

--- Vertical Prototype/prototype.php
+++ Vertical Prototype/prototype.php
@@ -7,21 +7,22 @@
 
 <body>
     
-    <form method="post">
-        <input type="text" name="email"/>
-        <div></div>
-        <input type="text" name="username"/>
-        <div></div>
-        <input type="text" name="firstname"/>
-        <div></div>
-        <input type="text" name="lastname"/>
-        <div></div>
-        <input type="text" name="password"/>
-        <div></div>
+    <form action="insert.php" method="post">
+        <input type="text" name="email" placeholder="email"/>
+        <br>
+        <input type="text" name="username" placeholder="username"/>
+        <br>
+        <input type="text" name="firstname" placeholder="firstname"/>
+        <br>
+        <input type="text" name="lastname" placeholder="lastname"/>
+        <br>
+        <input type="text" name="password" placeholder="password"/>
+        <br>
         <input type="submit" value="Insert"/>
     </form>
 
     <form method="post">
+        <div><p>Enter a users firstname to search for last name and email</p></div>
         <input type="text" name="search" required/>
         <input type="submit" value="Search"/>
     </form>
@@ -34,7 +35,7 @@
             foreach($results as $r){
                 echo "<div>" . $r['lastname'] . " - " . $r['email'] . "</div>";
             }
-        } else {echo "<div>No results found.</div>"}
+        } else {echo "<div>No results found.</div>";}
     }
     ?>
 </body>
