8514818fff8f52c80214765a4a47273b19f10f31
BryceKurek <89614933+BryceKurek@users.noreply.github.com>
2021-10-22 15:17:51 -0400

    Update prototype.php

--- Vertical Prototype/prototype.php
+++ Vertical Prototype/prototype.php
@@ -6,6 +6,20 @@
 </head>
 
 <body>
+    
+    <form method="post">
+        <input type="text" name="email"/>
+        <div></div>
+        <input type="text" name="username"/>
+        <div></div>
+        <input type="text" name="firstname"/>
+        <div></div>
+        <input type="text" name="lastname"/>
+        <div></div>
+        <input type="text" name="password"/>
+        <div></div>
+        <input type="submit" value="Insert"/>
+    </form>
 
     <form method="post">
         <input type="text" name="search" required/>
@@ -18,7 +32,7 @@
         if(count($results) > 0)
         {
             foreach($results as $r){
-                echo "<div>" . $r['lastname' . " - " . $r['email'] . "</div>"]
+                echo "<div>" . $r['lastname'] . " - " . $r['email'] . "</div>";
             }
         } else {echo "<div>No results found.</div>"}
     }
