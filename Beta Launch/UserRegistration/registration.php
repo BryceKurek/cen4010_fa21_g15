c0601a29b68cbf0b12bc7bed1b0c090c46c46b4a
kfruda <52004052+BeeAdmin12@users.noreply.github.com>
2021-11-05 23:14:14 -0400

    Html update via James
    Merged James' Html with the php files to create a functioning, good looking login and registration page
    
    Co-Authored-By: jgoedmakers <89616051+jgoedmakers@users.noreply.github.com>

--- Beta Launch/UserRegistration/registration.php
+++ Beta Launch/UserRegistration/registration.php
@@ -1,17 +1,19 @@
 <?php
 
 session_start();
-header('location:login.php');#This location should change to home html page when implemented. Or maybe back to login page
+header('location:login.php');#relocation if php runs successfully
 
 $con = mysqli_connect('localhost', 'cen4010_fa21_g15', 'N55POhE+OF', 'cen4010_fa21_g15');
 
 mysqli_select_db($con, "cen4010_fa21_g15");
 
+#naming linked to name value in html code
 $name = $_POST['user'];
 $pass = $_POST['password'];
 $email = $_POST['email'];
 $first = $_POST['firstname'];
 $last = $_POST['lastname'];
+$pass_check = $_POST['password_check'];
 
 $s = " select * from user where username = '$name'";
 
@@ -19,12 +21,17 @@ $result = mysqli_query($con, $s);
 
 $num = mysqli_num_rows($result);
 
+#Username and Password Verification
 if($num == 1) {
     echo"username already taken";
 } else {
-    $reg = "insert into user(email, username, firstname, lastname, password) values ('$email', '$name', '$first', '$last', '$pass')";
-    mysqli_query($con, $reg);
-    echo"Registration Successful";
+    if($pass == $pass_check) {
+        $reg = "insert into user(email, username, firstname, lastname, password) values ('$email', '$name', '$first', '$last', '$pass')";
+        mysqli_query($con, $reg);
+        echo"Registration Successful";
+    } else {
+        echo"passwords were not identical";
+    }
 }
 
 ?>
