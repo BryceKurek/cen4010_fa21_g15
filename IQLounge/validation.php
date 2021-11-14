e1cb5c8efb73d3c7a947d26cedb223068a1e2351
BryceKurek <89614933+BryceKurek@users.noreply.github.com>
2021-11-13 21:23:16 -0500

    hashing encryption

--- IQLounge/validation.php
+++ IQLounge/validation.php
@@ -7,16 +7,21 @@ mysqli_select_db($con, "cen4010_fa21_g15");
 $name = $_POST['user'];
 $pass = $_POST['password'];
 
-$s = " select * from user where username = '$name' && password = '$pass'";
-
+$s = "SELECT * FROM user WHERE (username = '$name')";
 $result = mysqli_query($con, $s);
 
-$num = mysqli_num_rows($result);
-
-if($num == 1) {
-    header('location:index.html'); #go to home page of project. name may change
-} else {
-    header('location:login.php');
+if(!$result){
+    die(mysqli_error($con));
 }
+if(mysqli_num_rows($result) > 0)
+{
+    $data = mysqli_fetch_array($result);
 
+
+    if(password_verify($pass, $data['password'])) {
+        header('location:index.html'); #go to home page of project. name may change
+    } else {
+        header('location:login.php');
+    }
+}
 ?>
